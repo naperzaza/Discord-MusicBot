4f0d5ecdfa2290a775589164ade8d6e82b861789
naperzaza <91073323+naperzaza@users.noreply.github.com>
2021-09-20 21:10:40 +0700

    Config.js

--- botconfig.js
+++ botconfig.js
@@ -4,13 +4,13 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
+  Token: process.env.Token || "ODg5NTA2MDE5NDQzNjMwMTEy.YUiPCQ.KDL2qAgIshT3WXQGBghBO2BX1aE", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "889506019443630112", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Tooyen is yen", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -29,7 +29,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "f57ff2ea0b7c4016a66c89b0ac2dc2b8", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "b304d5536c3443a8a6bf888675346d2a", //Spotify Client Secret
   },
 };
